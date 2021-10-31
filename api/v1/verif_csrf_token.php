713ad74bc86e07795ffe35361fb0d77c2d7c7d64
lotfio <contact@lotfio.net>
2021-10-31 14:46:04 +0100

    fix csrf validation

--- api/v1/verif_csrf_token.php
+++ api/v1/verif_csrf_token.php
@@ -1,10 +1,9 @@
 <?php
 
 
-$from = !empty($_GET['from'])? clean_var($_GET['from']):clean_var($_POST['from']);;
-$csrf_token = !empty($_GET['csrf_token'])? clean_var($_GET['csrf_token']):clean_var($_POST['csrf_token']);
-$user_csrf_token = get_user_csrf_token($user_id);
-
+$from               = $_REQUEST['from'] ?? '';
+$csrf_token         = $_REQUEST['csrf_token'] ?? '';
+$session_csrf_token = $_SESSION['csrf_token']; // removed csrf check from db |and used session based csrf
 
 $array_reponse = array( "errors" => [
                                       'csrf_token' => ''
@@ -12,10 +11,8 @@ $array_reponse = array( "errors" => [
                         'success'=>'yes' );
 
 //csrf_token
-if(($csrf_token == '')
-    
-    || ($csrf_token != $user_csrf_token)
-) {
+if(($csrf_token == '') || ($csrf_token != $session_csrf_token)) {
+
     $array_reponse["errors"]['csrf_token'] =  'Vous n\'avez pas de permission!';
     $array_reponse['success'] = "no";
 }
@@ -27,8 +24,3 @@ if($array_reponse['success']!="yes") {
 }else {
     $array_reponse = array();
 }
-
-// echo '$csrf_token = '.$csrf_token.'<br>';
-// echo '$user_csrf_token = '.$user_csrf_token.'<br>';
-// echo '$user_id = '.$user_id.'<br>';
-// echo '$from = '.$from.'<br>';
