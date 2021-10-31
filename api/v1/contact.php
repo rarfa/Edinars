db0e8c582506427ef5dba772a240e6b86259d676
lotfio <contact@lotfio.net>
2021-10-31 21:02:33 +0100

    fix contact page

--- api/v1/contact.php
+++ api/v1/contact.php
@@ -1,59 +1,53 @@
 <?php
+
 header('Content-Type: application/json');
 
 define("DIR_ROOT", "../../");
 
-require DIR_ROOT.'includes/All_files.php';
-
-// security
+require DIR_ROOT . 'includes/All_files.php';
 
 
+$name       = isset($_REQUEST['name']) ? clean_var($_REQUEST['name']) : '';
+$email      = isset($_REQUEST['email']) ? clean_var($_REQUEST['email']) : '';
+$subject    = isset($_REQUEST['subject']) ? clean_var($_REQUEST['subject']) : '';
+$message    = isset($_REQUEST['message']) ? clean_var($_REQUEST['message']) : '';
 
-// $user = new user();
+$array = array( 'name'      => '',
+                'email'     => '',
+                'subject'   => '',
+                'message'   => '',
 
-// http://www.edinars.net/api/v1/contact.php?name=test&email=testp&subject=&message=
+                'contact'   => '',
+                'success'   => 'yes'
+            );
 
-$name= $_GET['name']? clean_var($_GET['name']): clean_var($_POST['name']);
-$email = clean_var($_GET['email']);
-$subject = clean_var($_GET['subject']);
-$message = ($_GET['message']);
-
-$array = array( 'name' => '',
-                'email' => '',
-                'subject' => '',
-                'message' => '',
-
-                'contact' => '',
-                'success'=>'yes' );
-
-//name
-if($name== '') {
+// name
+if ($name == '') {
     $array['name'] =  'Le nom ne peut pas être vide!';
     $array['success'] = "no";
 }
 
 //email
-if($email == '') {
+if ($email == '') {
     $array['email'] = 'Email ne peut pas être vide!';
     $array['success'] = "no";
 }
 
 //subject
-if($subject == '') {
+if ($subject == '') {
     $array['subject'] = 'le sujet ne peut pas être vide!';
     $array['success'] = "no";
 }
 
 //message
-if($message == '') {
+if ($message == '') {
     $array['message'] = 'Le message ne peut pas être vide!';
     $array['success'] = "no";
 }
 
 //envoyer email ()
-if($array['success']=="yes") {
-    // Plusieurs destinataires
-    $to  = 'info@edinars.net,info@createam-dz.com';
+if ($array['success'] == "yes") {
+    $to     = 'contact@erecovery.net';
     // $to  = 'yacineaitchalal@gmail.com';
 
     // Sujet
@@ -65,31 +59,17 @@ if($array['success']=="yes") {
     $message_html = '
   <html>
   <head>
-    <title>'.$subject.'</title>
+    <title>' . $subject . '</title>
   </head>
   <body>
-   <p>Nom : <b>'.$name.'</b> </p>
-   <p>Email : <b>'.$email.'</b> </p>
-   <p>Message : <b>'.$message.'</b> </p>
+   <p>Nom : <b>' . $name . '</b> </p>
+   <p>Email : <b>' . $email . '</b> </p>
+   <p>Message : <b>' . $message . '</b> </p>
   </body>
   </html>
   ';
-
-    // Pour envoyer un mail HTML, l'en-tête Content-type doit être défini
-    $headers  = 'MIME-Version: 1.0' . "\r\n";
-    $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-
-    // En-têtes additionnels
-    //  $headers .= 'To: Mary <mary@example.com>, Kelly <kelly@example.com>' . "\r\n";
-    $headers .= 'From: Contact formulaire <infos@edinars.net>' . "\r\n";
-    $headers .= 'Reply-To: <'.$email.'>' . "\r\n";
-    //  $headers .= 'Cc: anniversaire_archive@edinars.net' . "\r\n";
-    //  $headers .= 'Bcc: anniversaire_verif@edinars.net' . "\r\n";
-
     // Envoi
-    mail($to, $subject, $message_html, $headers);
+    sendMail($to, $subject, $message_html);
 }
-//error = 0
-
 
 echo json_encode($array);
