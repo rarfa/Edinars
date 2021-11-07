1d6f02761a13edd8eacb8b528aaa41a82fc61b9c
m0unir17 <takimunir@gmail.com>
2021-11-07 11:49:18 +0100

    FIX qr code

--- api/v1/qr.php
+++ api/v1/qr.php
@@ -8,10 +8,10 @@ require DIR_ROOT.'includes/All_files.php';
 
 // security
 // include('verif_csrf_token.php');
-require 'verif_user.php';
+//require 'verif_user.php';
 
 
-if($_GET["qr_type"]=="identity") {
+if(isset($_GET["qr_type"]) && $_GET["qr_type"] == "identity") {
     $array_imp =db_rows(
         "SELECT * FROM `{$data['DbPrefix']}members`".
         "WHERE `id`={$user_id}"
@@ -19,11 +19,13 @@ if($_GET["qr_type"]=="identity") {
 
     $qr_str = $array_imp[0]['mem_id'];
 
-}elseif($_GET["qr_type"]=="order") {
+}elseif(isset($_GET["qr_type"]) && $_GET["qr_type"] == "order") {
     $qr_str = clean_var($_GET['trx_id']);
 }
 
 
+if(isset($qr_str)){
+    QRcode::png($qr_str, false, 20, 10, false);
+}
 
-QRcode::png($qr_str, false, 20, 10, false);
 
