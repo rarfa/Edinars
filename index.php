ec351c35e238454308eacfc409d3d89e62baffff
m0unir17 <takimunir@gmail.com>
2021-11-14 11:46:57 +0100

    fix tawk

--- index.php
+++ index.php
@@ -50,7 +50,7 @@ var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
 (function(){
 var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
 s1.async=true;
-s1.src='https://embed.tawk.to/5a4eacebd7591465c70680ff/default';
+s1.src='https://embed.tawk.to/6190da7b6885f60a50bbb795/default';
 s1.charset='UTF-8';
 s1.setAttribute('crossorigin','*');
 s0.parentNode.insertBefore(s1,s0);
